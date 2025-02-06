3a67196a879c0fcf6caf85333d53bb7d74eb69f6
slavgetov <preslavcodes@abv.bg>
2025-02-06 14:22:12 +0200

    signal

--- src/app/paragraph/paragraph.component.ts
+++ src/app/paragraph/paragraph.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-
+import { signal } from '@angular/core';
 @Component({
   selector: 'app-paragraph',
   standalone: true,
   imports: [],
   templateUrl: './paragraph.component.html',
-  styleUrl: './paragraph.component.css'
+  styleUrl: './paragraph.component.css',
 })
 export class ParagraphComponent {
-
+  personalName = signal('Slav');
 }
