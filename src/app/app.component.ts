410619978cd7db5a0204644c8bda27c0ae7edeca
slavgetov <preslavcodes@abv.bg>
2025-02-06 13:51:36 +0200

    add our first paragraph

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { ParagraphComponent } from './paragraph/paragraph.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ParagraphComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'ng-commands';
